9348464d4d7f6e237e875ca88a71de3df1e5885c
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-04-16 20:37:45 -0600

    completed header component with auth functionality

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Header extends Component {
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+        return
+      case false:
+        return (
+          <li><a href="/auth/google">Login With Google</a></li>
+        )
+      default:
+        return (
+        <li><a href="/api/logout">Logout</a></li>
+        )
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a href="/" className="left brand-logo">Emaily</a>
           <ul className="right">
-            <li><a href="/auth/google">Login With Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +31,8 @@ class Header extends Component {
   }
 }
 
-export default Header
+const mapStateToProps = ({ auth }) => ({
+  auth
+})
+
+export default connect(mapStateToProps)(Header)
