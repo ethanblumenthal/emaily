ab386507b8355d2441930aeebb1877b46e7caea0
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-04-19 10:38:46 -0500

    completed functionality to send surveys

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { reduxForm } from 'redux-form'
 import SurveyForm from './SurveyForm'
 import SurveyReview from './SurveyReview'
 
@@ -21,4 +22,4 @@ class SurveyNew extends Component {
   }
 }
 
-export default SurveyNew
+export default reduxForm({ form: 'surveyForm' })(SurveyNew)
--- client/src/components/surveys/SurveyReview.js
+++ client/src/components/surveys/SurveyReview.js
@@ -1,9 +1,11 @@
 import _ from 'lodash'
 import React from 'react'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 import formFields from './formFields'
+import { submitSurvey } from '../../actions'
 
-const SurveyReview = ({ onCancel, formValues }) => {
+const SurveyReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
       <div key={name}>
@@ -17,7 +19,11 @@ const SurveyReview = ({ onCancel, formValues }) => {
     <div>
       <h5>Please confirm your entries</h5>
       {reviewFields}
-      <button className="yellow darken-3 btn-flat" onClick={onCancel}>Back</button>
+      <button className="yellow darken-3 btn-flat white-text" onClick={() => onCancel}>Back</button>
+      <button className="green btn-flat right white-text" onSubmit={submitSurvey(formValues, history)}>
+        Send Survey
+        <i className="material icons right">email</i>  
+      </button>
     </div>
   )
 }
@@ -26,4 +32,4 @@ const mapStateToProps = state => ({
   formValues: state.form.surveyForm.values
 })
 
-export default connect(mapStateToProps)(SurveyReview)
+export default connect(mapStateToProps, { submitSurvey })(withRouter(SurveyReview))
